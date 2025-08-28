d7b3cf310bdca97354384a82f36e1d38ad505e5b
LucasCid <lucascidcanete@gmail.com>
2025-08-28 16:14:15 -0400

    xd

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: 'https://lucascid.github.io/aplicacion-gastos',
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lucascid.github.io/aplicacion-gastos',
+  // Quita cualquier línea que diga "base: '/aplicacion-gastos/'" o similar
 })
